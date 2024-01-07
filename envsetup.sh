1c8cf9895c98fcac6d1135ef13eb33007351245d
Red Prez <35945472+RedPrez16@users.noreply.github.com>
2024-01-07 19:16:55 +0000

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -103,7 +103,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 
 EOF
 
-    __print_derp_functions_help
+    __print_redbird_functions_help
 
 cat <<EOF
 
@@ -116,7 +116,7 @@ EOF
     local T=$(gettop)
     local A=""
     local i
-    for i in `cat $T/build/envsetup.sh $T/vendor/derp/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
+    for i in `cat $T/build/envsetup.sh $T/vendor/redbird/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
       A="$A $i"
     done
     echo $A
@@ -127,8 +127,8 @@ function build_build_var_cache()
 {
     local T=$(gettop)
     # Grep out the variable names from the script.
-    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/derp/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
-    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/derp/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/redbird/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/redbird/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
     # Call the build system to dump the "<val>=<value>" pairs as a shell script.
     build_dicts_script=`\builtin cd $T; build/soong/soong_ui.bash --dumpvars-mode \
                         --vars="${cached_vars[*]}" \
@@ -210,12 +210,12 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-    if (echo -n $1 | grep -q -e "^derp_") ; then
-        DERP_BUILD=$(echo -n $1 | sed -e 's/^derp_//g')
+    if (echo -n $1 | grep -q -e "^redbird_") ; then
+        REDBIRD_BUILD=$(echo -n $1 | sed -e 's/^redbird_//g')
     else
-        DERP_BUILD=
+        REDBIRD_BUILD=
     fi
-    export DERP_BUILD
+    export REDBIRD_BUILD
 
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
@@ -837,13 +837,13 @@ function lunch()
         # if we can't find a product, try to grab it off the DerpFest GitHub
         T=$(gettop)
         cd $T > /dev/null
-        vendor/derp/build/tools/roomservice.py $product
+        vendor/redbird/build/tools/roomservice.py $product
         cd - > /dev/null
         check_product $product
     else
         T=$(gettop)
         cd $T > /dev/null
-        vendor/derp/build/tools/roomservice.py $product true
+        vendor/redbird/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
 
@@ -889,7 +889,7 @@ function lunch()
     set_stuff_for_environment
 
     echo "";
-    cat $(gettop)/build/make/derp_ascii_logo;
+    cat $(gettop)/build/make/redbird_ascii_logo;
     echo"";
 
     [[ -n "${ANDROID_QUIET_BUILD:-}" ]] || printconfig
@@ -2145,4 +2145,4 @@ fi
 
 export ANDROID_BUILD_TOP=$(gettop)
 
-. $ANDROID_BUILD_TOP/vendor/derp/build/envsetup.sh
+. $ANDROID_BUILD_TOP/vendor/redbird/build/envsetup.sh
